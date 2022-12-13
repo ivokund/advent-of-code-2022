6afe5c673f2d503f5c4562d22f2461a2e0266e11
ivokund <ivo@pactum.com>
2022-12-13 21:54:30 +0200

    Day 13, part 2

--- day13/day13.ts
+++ day13/day13.ts
@@ -26,55 +26,46 @@ const input = Deno.readTextFileSync('./input.txt')
 type Packet = (number | Packet)[]
 type Pair = Packet[]
 
+const isRightOrder = ([left, right]: Pair): boolean | null => {
+  if (Number.isInteger(left) && Number.isInteger(right)) {
+    if (left === right) {
+      return null
+    } else if (left < right) {
+      return true
+    } else if (right < left) {
+      return false
+    }
+  }
+
+  const l = Array.isArray(left) ? left : [left]
+  const r = Array.isArray(right) ? right : [right]
+
+  const maxLen = Math.max(l.length, r.length)
+
+  for (let i=0; i<maxLen; i++) {
+    if (l[i] === undefined) {
+      return true
+    }
+
+    if (r[i] === undefined) {
+      // right ran out of items
+      return false
+    }
+    const subRight = isRightOrder([l[i] as Packet, r[i] as Packet])
+    if (subRight !== null) {
+      return subRight
+    }
+  }
+  return null
+}
+
 function part1(input: string) {
   const pairs: Pair[] = input.split(`\n\n`)
     .map((lines) => lines.split(`\n`)
       .map((line) => JSON.parse(line)))
 
-  const isRight = ([left, right]: Pair, padding: number): boolean | null => {
-    console.log(`${' '.repeat(padding)}- Compare ${JSON.stringify(left)} vs ${JSON.stringify(right)}`)
-    if (Number.isInteger(left) && Number.isInteger(right)) {
-      console.log(`${' '.repeat(padding+2)}- Compare ${left} vs ${right}`)
-
-      if (left === right) {
-        return null
-      } else if (left < right) {
-        console.log(`${' '.repeat(padding+4)}- Left side is smaller, RIGHT order`)
-        return true
-      } else if (right < left) {
-        console.log(`${' '.repeat(padding+4)}- Right side is smaller, NOT RIGHT order`)
-        return false
-      }
-    }
-
-    const l = Array.isArray(left) ? left : [left]
-    const r = Array.isArray(right) ? right : [right]
-
-    const maxLen = Math.max(l.length, r.length)
-
-    for (let i=0; i<maxLen; i++) {
-      if (l[i] === undefined) {
-        console.log(`${' '.repeat(padding+4)}- Left side ran out of items, RIGHT order`)
-        return true
-      }
-
-      if (r[i] === undefined) {
-        console.log(`${' '.repeat(padding+4)}- Right side ran out of items, NOT RIGHT order`)
-        // right ran out of items
-        return false
-      }
-      const subPair: Pair = [l[i] as Packet, r[i] as Packet]
-      const subRight = isRight(subPair, padding+2)
-      if (subRight !== null) {
-        return subRight
-      }
-    }
-    return null
-  }
-
   const values: number[] = pairs.reduce((acc: number[], pair, key) => {
-    console.log(`== Pair ${key+1} ==`)
-    if (isRight(pair, 0)) {
+    if (isRightOrder(pair)) {
       acc.push(key+1)
     }
     return acc
@@ -87,15 +78,26 @@ function part1(input: string) {
 function part2(input: string) {
   const packets: Packet[] = input.split(/\n+/)
       .map((line) => JSON.parse(line))
+  packets.push([[2]])
+  packets.push([[6]])
 
-  return packets
+  packets.sort((a, b) => {
+    const isRight = isRightOrder([a, b])
+    return isRight === null ? 0 : (isRight ? -1 : 1)
+  })
+
+  const serialized = packets.map((packet) => JSON.stringify(packet))
+  const index1 = serialized.findIndex((line) => line === '[[2]]')
+  const index2 = serialized.findIndex((line) => line === '[[6]]')
+
+  return (index1 + 1) * (index2 + 1)
 }
 
 
 console.log('-- test input')
 console.log({ part1: part1(testInput) })
-// console.log({ part2: part2(testInput) })
+console.log({ part2: part2(testInput) })
 
 console.log('-- real input')
 console.log({ part1: part1(input) })
-// console.log({ part2: part2(input) })
+console.log({ part2: part2(input) })
