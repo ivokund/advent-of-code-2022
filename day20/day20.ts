837c80dbc0a7e951108fccca95057f922b748fb5
ivokund <ivo@pactum.com>
2022-12-21 18:31:20 +0200

    Day 20, part 1

--- day20/day20.ts
+++ day20/day20.ts
@@ -4,118 +4,171 @@ const testInput = `1
 3
 -2
 0
-4
-16`
+4`
 const input = Deno.readTextFileSync('./input.txt')
 
-interface Num {
-  value: number
+interface ListItem {
   origValue: number
   moved: boolean
+  next?: ListItem
+  prev?: ListItem
+}
+const DEBUG = false
+
+const _cl = console.debug
+console.debug = (msg: string) => {
+  if (DEBUG) {
+    _cl(msg)
+  }
 }
 
-function part1(input: string) {
-  const values = input.split('\n').map(Number)
-  const len = values.length
-  const getIndex = (idx: number) => (idx + 2 * len) % len
+const prevBy = (element: ListItem, count: number) => {
+  const len = Math.abs(count)
+  for (let i=0; i<=len; i++) {
+    element = element.prev!
+  }
+  return element
+}
+const nextBy = (element: ListItem, count: number) => {
+  const len = Math.abs(count)
+  for (let i=0; i<len; i++) {
+    element = element.next!
+  }
+  return element
+}
 
-  const nums: Num[] = values.map((origValue) => {
-    let value = getIndex(origValue)
-    if (origValue < 0) {
-      value--
-    }
+const moveBy = (element: ListItem, count: number) => {
+  if (count > 0) {
+    return nextBy(element, count)
+  } else {
+    return prevBy(element, count)
+  }
+}
+
+// const draw = (label: string, head: ListItem) => {
+//   const items = []
+//   for (let i=0; i<len; i++) {
+//     items.push(head.origValue)
+//     head = head.next!
+//   }
+//   console.debug(label + items.join(', '))
+// }
+
+const createList = (values: number[]): ListItem[] => {
+  const len = values.length
+  const list: ListItem[] = values.map((origValue) => {
     return {
       origValue,
-      value,
       moved: false,
     }
   })
 
-  console.log({nums})
-
-  const getFirstUnmovedIndex = (): number => {
-    return nums.findIndex((val) => val.moved === false)
+  for (let idx = 0; idx < len; idx++) {
+    list[idx].prev = list[idx-1]
+    list[idx].next = list[idx+1]
   }
-  //
-  //
-  // console.log({max: Math.max(...values)})
-  // console.log({min: Math.min(...values)})
-  // console.log({len: values.length})
-  // console.log({nums})
-
-  let elementIdx = getFirstUnmovedIndex()
-
-  let i=1
-  while (elementIdx !== -1) {
-    console.log(`> ROUND ${i++}`)
-    // console.log(`  > START: ` + nums.map(({value}) => value).join(', '))
-
-    const element = nums[elementIdx]
-
-    // console.log(`  > Moving ${origValue}, really using ${value}`)
-    if (element.value < 0) {
-      throw new Error('wtf')
-    }
-
-    // console.log(`    > Adding elementIdx (${elementIdx}) + value (${value}) + ${2 * len}`)
-    let newIdx = (elementIdx + element.value) + len
-    // console.log(`    > New index is ${newIdx}`)
-    newIdx %= len
-    // console.log(`       > Taking modulo ${len}: ${newIdx}`)
-    // console.log(`    > Moving between ${nums[newIdx]?.value} and ${nums[newIdx+1]?.value}`)
-
-    // console.log(`      > Adding ${value} to splice index ${newIdx+1}`)
-
-    // if (newIdx === -1) {
-    //   newIdx = len
-    // }
-
-    nums.splice(newIdx + 1, 0, {
-      ...element,
-      moved: true
-    })
-    // console.log(`         > AFTER ADDING: ` + nums.map(({value}) => value).join(', '))
-
-
-    let deleteIndex = elementIdx
-
-    if (newIdx < elementIdx) {
-      // console.log('INCREASING DELETE INDEX')
-      deleteIndex++
-    }
-
-    // console.log(`      > Deleting ${nums[deleteIndex].value} from index ${deleteIndex}`)
-    nums.splice(deleteIndex, 1)
-
-    // console.log(`  > END: ` + nums.map(({value}) => value).join(', '))
-if (i>7) {
-  // Deno.exit()
+  list[0].prev = list[len-1]
+  list[len-1].next = list[0]
+  return list
 }
-    elementIdx = getFirstUnmovedIndex()
+
+const mix = (list: ListItem[], head: ListItem) => {
+  const len = list.length
+
+  const getFirstUnmovedElement = (): ListItem | undefined => {
+    let cursor = head
+    let i=0
+    while (i++ < len) {
+      if (!cursor.moved) {
+        return cursor
+      }
+      cursor = cursor.next!
+    }
   }
 
-  console.log({nums})
-  const zeroValIdx = nums.findIndex(({value}) => value === 0)
+  let element = getFirstUnmovedElement()
 
-  const returnValues = [1000, 2000, 3000].map((idx) => {
-    const index = getIndex(idx + zeroValIdx)
-    return nums[index].origValue
-  })
+  do {
+    if (!element) {
+      throw new Error('for compiler')
+    }
 
-  console.log({returnValues})
+    element.moved = true
+
+    if (element.origValue === 0) {
+      element = getFirstUnmovedElement()
+      continue
+    }
+
+    const prev = element.prev!
+    const next = element.next!
+
+    prev.next = next
+    next.prev = prev
+
+    const futurePrev = moveBy(element, element.origValue)
+    const futureNext = futurePrev.next!
+
+    element.next = futureNext
+    element.prev = futurePrev
+
+    next.prev = prev
+    prev.next = next
+
+    futurePrev.next = element
+    futureNext.prev = element
+
+    if (element === head) {
+      head = next
+    }
+
+    element = getFirstUnmovedElement()
+    // draw(`  > END: `)
+
+  } while (element)
+}
+
+const getAnswer = (list: ListItem[]) => {
+  const zeroValIdx = list.findIndex(({origValue}) => origValue === 0)
+  const val1 = nextBy(list[zeroValIdx], 1000)
+  const val2 = nextBy(val1, 1000)
+  const val3 = nextBy(val2, 1000)
+
+  const returnValues = [val1.origValue, val2.origValue, val3.origValue]
   return returnValues.reduce((sum, val) => sum + val)
 }
 
+function part1(input: string) {
+  const values = input.split('\n').map(Number)
+  const list = createList(values)
+  mix(list, list[0])
+  return getAnswer(list)
+}
+
 function part2(input: string) {
+  const KEY = 811589153
+  const values = input.split('\n')
+    .map((val) => (Number(val) * KEY))
+
+  const list = createList(values)
+
+  const unMix = () => list.forEach((item) => {
+    item.moved = false
+  })
+
+  for (let mixCount = 0; mixCount < 10; mixCount++) {
+    console.log({mixCount})
+    mix(list, list[0])
+    unMix()
+  }
+
   return input
 }
 
-// 14813
-// 4813
 console.log('-- test input')
 console.log({ part1: part1(testInput) })
 // console.log({ part2: part2(testInput) })
 
 console.log('-- real input')
-// console.log({ part1: part1(input) })
+console.log({ part1: part1(input) })
 // console.log({ part2: part2(input) })
