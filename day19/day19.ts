0de88c4b1c4e2b6671da8861f7d1f21194e4f216
ivokund <ivo@pactum.com>
2023-01-13 17:51:50 -0700

    Day 19, part 2, slow

--- day19/day19.ts
+++ day19/day19.ts
@@ -14,9 +14,7 @@ interface QueueItem {
   materialCounts: CountByMaterial
   robotCounts: CountByMaterial
   blueprint: Blueprint
-  buildHistory: RobotFactoryAction[]
   actionCount: number
-  debugLog: string[]
 }
 
 const blankCosts: CountByMaterial = {
@@ -25,7 +23,11 @@ const blankCosts: CountByMaterial = {
   obsidian: 0,
   geode: 0,
 }
-
+const incrementMaterials = (materialCounts: CountByMaterial, robotCounts: CountByMaterial) => {
+  Object.keys(robotCounts).forEach((robot: Material) => {
+    materialCounts[robot] = (materialCounts[robot] ?? 0) + 1
+  })
+}
 const subtractMaterials = (materials: CountByMaterial, materials2: CountByMaterial): CountByMaterial => {
   return {
     ore: (materials['ore'] ?? 0) - (materials2['ore'] ?? 0),
@@ -34,19 +36,8 @@ const subtractMaterials = (materials: CountByMaterial, materials2: CountByMateri
     geode: (materials['geode'] ?? 0) - (materials2['geode'] ?? 0),
   }
 }
-// const subtractMaterials = (materials: CountByMaterial, materials2: CountByMaterial): CountByMaterial => {
-//   return Object.fromEntries(Object.entries(materials).map(([material, counts]) => {
-//     return [material, counts - materials2[material as Material] ?? 0]
-//   })) as CountByMaterial
-// }
 
-const incrementMaterials = (materialCounts: CountByMaterial, robotCounts: CountByMaterial) => {
-  Object.keys(robotCounts).forEach((robot: Material) => {
-    materialCounts[robot] = (materialCounts[robot] ?? 0) + 1
-  })
-}
 const collectMaterials = (materialCounts: CountByMaterial, robotCounts: CountByMaterial): CountByMaterial => {
-
   return Object.fromEntries(Object.keys(robotCounts).map((robot) => {
     return ([robot, (materialCounts[robot as Material] || 0) + robotCounts[robot as Material]])
   })) as CountByMaterial
@@ -55,11 +46,9 @@ const collectMaterials = (materialCounts: CountByMaterial, robotCounts: CountByM
 
 type RobotFactoryAction = Material | 'none'
 type ActionsWithCosts = Partial<Record<RobotFactoryAction, CountByMaterial>>
-const getPossibleFactoryBuildActions = (materialCounts: CountByMaterial, blueprint: Blueprint, forceItem: RobotFactoryAction | undefined): ActionsWithCosts => {
+const getPossibleFactoryBuildActions = (materialCounts: CountByMaterial, blueprint: Blueprint): ActionsWithCosts => {
   const actionsAndLeftoverMaterial: ActionsWithCosts = Object.entries(blueprint.robotCosts)
     .reduce((buildItems: Record<RobotFactoryAction, CountByMaterial>, [robot, costs]) => {
-
-      if (forceItem && robot !== forceItem) { return buildItems }
       const leftovers = subtractMaterials(materialCounts, costs)
       const overLimits = Object.entries(leftovers).filter(([, count]) => count < 0)
       if (overLimits.length === 0) {
@@ -67,15 +56,13 @@ const getPossibleFactoryBuildActions = (materialCounts: CountByMaterial, bluepri
       }
       return buildItems
     }, {})
-  if (!forceItem || forceItem === 'none'){
-    actionsAndLeftoverMaterial['none'] = materialCounts
-  }
+  actionsAndLeftoverMaterial['none'] = materialCounts
 
   return actionsAndLeftoverMaterial
 }
 
-function part1(input: string) {
-  const blueprints: Blueprint[] = input.split('\n').map((line) => {
+const parseBlueprints = (input: string): Blueprint[] => {
+  return input.split('\n').map((line) => {
     const matches = line.match(/(\d+)/g)!.map(Number)
     return ({
       no: matches[0],
@@ -87,79 +74,34 @@ function part1(input: string) {
       },
     })
   })
+}
 
+const run = (blueprints: Blueprint[], minutes: number) => {
   const robotCounts = { ...blankCosts, ore: 1 }
   const materialCounts = { ...blankCosts }
-
 
   const queue: QueueItem[] = blueprints.map((blueprint) => {
     return {
       blueprint,
       robotCounts: {...robotCounts},
       materialCounts: {...materialCounts},
-      buildHistory: [],
       actionCount: 0,
       debugLog: []
     }
   })
 
-  // const mock: (RobotFactoryAction)[] = [
-  //   'none',
-  //   'none',
-  //   'clay',
-  //   'none',
-  //   'clay',
-  //   'none',
-  //   'clay',
-  //   'none',
-  //   'none',
-  //   'none',
-  //   'obsidian',
-  //   'clay',
-  //   'none',
-  //   'none',
-  //   'obsidian',
-  //   'none',
-  //   'none',
-  //   'geode',
-  //   'none',
-  //   'none',
-  //   'geode',
-  //   'none',
-  //   'none',
-  //   'none',
-  // ]
-
-  let maxGeodes = 0
-  let maxGeodesHistory: RobotFactoryAction[] = []
-
   const maxGeodesByBlueprintId: Record<number, number> = {}
 
-  let i=0
   while (queue.length > 0) {
-    i++
-
     const queueItem = queue.pop()!
-
-    const history = queueItem.buildHistory.join('>')
-    if (i % 1000000 === 0) {
-      console.log(`> ${i}, MINUTE ${queueItem.buildHistory.length+1} (${history}), blueprint: ${queueItem.blueprint.no}, queue length: ${queue.length}, max: ${maxGeodes}`)
-
-      console.log({i, robots: queueItem.robotCounts, materials: queueItem.materialCounts})
-      console.log({maxGeodesByBlueprintId})
-    }
-
-    // const mockChoice = mock[queueItem.buildHistory.length]
 
     let toBuild = getPossibleFactoryBuildActions(
       queueItem.materialCounts,
       queueItem.blueprint,
-      undefined,
-      // mockChoice
     )
 
     // skip none and ore if can build something else
-    if (toBuild['ore'] && queueItem.materialCounts['ore'] > 10 && toBuild['clay']) {
+    if (toBuild['ore'] && queueItem.materialCounts['ore']! > 10 && toBuild['clay']) {
       delete toBuild['none']
       delete toBuild['ore']
     }
@@ -168,97 +110,60 @@ function part1(input: string) {
     if (toBuild['geode']) {
       toBuild = {geode: toBuild['geode']}
     }
-
-    if (queueItem.materialCounts['clay'] > 20 && toBuild['obsidian']) {
+    // if there's plenty of clay and we could build obsidian, do it
+    if (queueItem.materialCounts['clay']! > 20 && toBuild['obsidian']) {
       delete toBuild['none']
       delete toBuild['clay']
       delete toBuild['ore']
     }
 
-
-    if (i % 1000000 === 0) {
-      console.log({materialCounts:queueItem.materialCounts})
-      console.log({toBuild})
-    }
-
     Object.entries(toBuild).forEach(([factoryBuildAction, newMaterialCounts]) => {
-      const newLogItems = [`== Minute ${queueItem.buildHistory.length+1} ==`]
-
       const newRobotCounts = {...queueItem.robotCounts}
 
-      let material = null
       if (factoryBuildAction !== 'none') {
-        material = factoryBuildAction as Material
-        // console.log(`  > Action: ${factoryBuildAction}`)
-        newLogItems.push(`Spend ${JSON.stringify(queueItem.blueprint.robotCosts[material])} to start building a ${material}-collecting robot `)
-        newRobotCounts[factoryBuildAction as Material]++
+        newRobotCounts[factoryBuildAction as Material]!++
       }
 
-
       const newMaterialCounts2 = collectMaterials(newMaterialCounts, queueItem.robotCounts)
-      // incrementMaterials(newMaterialCounts, queueItem.robotCounts)
-      // Object.entries(newMaterialCounts).forEach(([material, newCount]) => {
-      //   const added = newCount - newMaterialCounts[material as Material]
-      //   newLogItems.push(`${queueItem.robotCounts[material as Material]} ${material}-collecting robots collect ${added} ${material}; you now have ${newCount} ${material} `)
-      // })
-
-      // const newHistory = [...queueItem.buildHistory, factoryBuildAction as RobotFactoryAction]
-      if (queueItem.actionCount < 24) {
-        // console.log(`  > New robots count: ${JSON.stringify(newRobotCounts)}`)
-        // console.log(`  > New material count: ${JSON.stringify(newMaterialCounts)}`)
-        // console.log(`  > History: ${JSON.stringify(newHistory)}`)
-        if (material) {
-          // newLogItems.push(`The new ${material}-collecting robot is ready. You now have ${newRobotCounts[material]} of them`)
-        } else {
-          // newLogItems.push(`Did not build anything this minute`)
-        }
+      if (queueItem.actionCount < minutes) {
         queue.push({
           blueprint: queueItem.blueprint,
           robotCounts: newRobotCounts,
           materialCounts: newMaterialCounts2,
           actionCount: queueItem.actionCount + 1,
-          buildHistory: [], //newHistory,
-          debugLog: [], //[...queueItem.debugLog, '', ...newLogItems],
         })
       } else {
-        // console.log({newMaterialCounts2})
-        if (newMaterialCounts.geode >= (maxGeodesByBlueprintId[queueItem.blueprint.no] ?? 0)) {
-          maxGeodesByBlueprintId[queueItem.blueprint.no] = newMaterialCounts.geode
+        if (newMaterialCounts.geode! >= (maxGeodesByBlueprintId[queueItem.blueprint.no] ?? 0)) {
+          maxGeodesByBlueprintId[queueItem.blueprint.no] = newMaterialCounts.geode!
         }
-        // if (newMaterialCounts.geode > maxGeodes) {
-        //   maxGeodes = newMaterialCounts.geode
-        //   maxGeodesHistory = newHistory
-        // }
       }
     })
-    // todo: skip wait if there are resources to build geode robots?
-    if (i>10) {
-      // Deno.exit()
-    }
   }
+  return maxGeodesByBlueprintId
+}
 
-  console.log('Done')
-  console.log({maxGeodesByBlueprintId})
+function part1(input: string) {
+  const blueprints = parseBlueprints(input)
+  const maxGeodesByBlueprintId = run(blueprints, 24)
+  const qualityLevels = Object.entries(maxGeodesByBlueprintId)
+    .map(([blueprint, max]) => Number(blueprint) * max)
 
-  const qualityLevels = Object.entries(maxGeodesByBlueprintId).map(([blueprint, max]) => {
-    return Number(blueprint) * max
-  })
-  console.log({qualityLevels})
-
-  const sum = qualityLevels.reduce((acc, level) => acc + level)
-  console.log({sum})
-  return sum
+  return qualityLevels.reduce((acc, level) => acc + level)
 }
 
 function part2(input: string) {
-  return input
+  const blueprints = parseBlueprints(input).slice(0, 3)
+  const maxGeodesByBlueprintId = run(blueprints, 32)
+
+  return Object.values(maxGeodesByBlueprintId)
+    .reduce((acc, max) => acc * max)
 }
 
 
 console.log('-- test input')
-// console.log({ part1: part1(testInput) })
-// console.log({ part2: part2(testInput) })
+console.log({ part1: part1(testInput) })
+console.log({ part2: part2(testInput) })
 
 console.log('-- real input')
 console.log({ part1: part1(input) })
-// console.log({ part2: part2(input) })
+console.log({ part2: part2(input) })
